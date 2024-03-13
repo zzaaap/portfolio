0d7f22868bfae9c202ab9956ed28ee266c98734c
zzaaap <iambazzy@gmail.com>
2024-03-13 06:58:55 -0500

    Upload contact splash page

---
+++ archive/static-scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $('.popup-btn').click(function(){ 
+    var popupBlock = $('#'+$(this).data('popup'));
+    popupBlock.addClass('active')
+      .find('.fade-out').click(function(){
+        popupBlock.css('opacity','0').find('.popup-content').css('margin-top','350px');        
+        setTimeout(function(){
+          $('.popup').removeClass('active');
+          popupBlock.css('opacity','').find('.popup-content').css('margin-top','');
+        }, 600);
+      });
+ });
+});
