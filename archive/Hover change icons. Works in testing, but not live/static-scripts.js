fdf08b255a2045fe7bb9c86e5c0b8da83bf5e9ae
zzaaap <iambazzy@gmail.com>
2024-03-13 22:51:42 -0500

    Committed working sample

---
+++ archive/Hover change icons. Works in testing, but not live/static-scripts.js
@@ -0,0 +1,34 @@
+/* Pop-Up Animation for Contact Form
+---------------------------------------------------------- */
+$(document).ready(function() {
+  
+	$('.btn-open').click(function() {
+		$('.modal-wrapper').addClass('open')
+	});
+  
+	$('.btn-close').click(function() {
+		$('.modal-wrapper').removeClass('open')
+	});
+  
+});
+
+/* Contact Form
+---------------------------------------------------------- */
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
