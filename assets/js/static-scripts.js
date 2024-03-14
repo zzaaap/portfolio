21beb85fc26c13326a65a3d680e25efc7c0550b9
zzaaap <iambazzy@gmail.com>
2024-03-13 22:47:27 -0500

    CSS hover attempt in Safari

--- assets/js/static-scripts.js
+++ assets/js/static-scripts.js
@@ -14,21 +14,3 @@ $(document).ready(function() {
 
 /* Contact Form
 ---------------------------------------------------------- */
-const inputs = document.querySelectorAll(".input");
-
-function focusFunc() {
-  let parent = this.parentNode;
-  parent.classList.add("focus");
-}
-
-function blurFunc() {
-  let parent = this.parentNode;
-  if (this.value == "") {
-    parent.classList.remove("focus");
-  }
-}
-
-inputs.forEach((input) => {
-  input.addEventListener("focus", focusFunc);
-  input.addEventListener("blur", blurFunc);
-});
